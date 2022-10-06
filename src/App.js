aed0c77e09a270645fd1e99a6345b11166d74674
Miguel <miguezaga2@hotmail.com>
2022-10-06 16:21:19 -0500

    add html classes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import { useState } from "react";
 
 function App() {
   return (
-    <article>
-      <img src="" alt="" />
-      <div>
-        <h1>
+    <article className="article">
+      <img className="article__img-top" src="" alt="" />
+      <div className="article__main">
+        <h1 className="article__title">
           Shift the overall look and feel by adding these wonderful touches to
           furniture in your home
         </h1>
-        <p>
+        <p className="article__text">
           Ever been in a room and felt like something was missing? Perhaps it
           felt slightly bare and uninviting. I’ve got some simple tips to help
           you make any room feel complete.
         </p>
       </div>
-      <div>Michelle Appleton 28 Jun 2020</div>
-      <ul>Share</ul>
+      <div className="article__author">Michelle Appleton 28 Jun 2020</div>
+      <ul className="article__socials">Share</ul>
     </article>
   );
 }
