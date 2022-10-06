da3c7bcabc0956f6ee71e24f7c9f5545b6f789b0
Miguel <miguezaga2@hotmail.com>
2022-10-06 15:57:03 -0500

    add given files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ function App() {
   return (
     <div>
       <h1>New App</h1>
+  Shift the overall look and feel by adding these wonderful 
+  touches to furniture in your home
+
+  Ever been in a room and felt like something was missing? Perhaps 
+  it felt slightly bare and uninviting. I’ve got some simple tips 
+  to help you make any room feel complete.
+
+  Michelle Appleton
+  28 Jun 2020
+
+  Share
     </div>
   );
 }
