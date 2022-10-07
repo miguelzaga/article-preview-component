4dd5b4e5d88ca6f7d6f284535e99512b90c043f0
Miguel <miguezaga2@hotmail.com>
2022-10-07 08:45:59 -0500

    add footer styling

--- README-template.md
+++ README-template.md
@@ -91,8 +91,8 @@ Use this section to outline areas that you want to continue focusing on in futur
 
 ### Useful resources
 
-- [Example resource 1](https://www.example.com) - This helped me for XYZ reason. I really liked this pattern and will use it going forward.
-- [Example resource 2](https://www.example.com) - This is an amazing article which helped me finally understand XYZ. I'd recommend it to anyone still learning this concept.
+- [https://www.digitalocean.com/community/tutorials/css-cropping-images-object-fit](https://www.example.com) - This helped me for XYZ reason. I really liked this pattern and will use it going forward.
+- [https://developer.mozilla.org/en-US/docs/Web/HTML/Element/time](https://www.example.com) - This is an amazing article which helped me finally understand XYZ. I'd recommend it to anyone still learning this concept.
 
 **Note: Delete this note and replace the list above with resources that helped you during the challenge. These could come in handy for anyone viewing your solution or for yourself when you look back on this project in the future.**
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import { useState } from "react";
+import img from "/src/images/drawers.jpg";
+import avatar from "/src/images/avatar-michelle.jpg";
 
 function App() {
   return (
-    <article className="article">
-      <img className="article__img-top" src="" alt="" />
-      <div className="article__main">
-        <h1 className="article__title">
-          Shift the overall look and feel by adding these wonderful touches to
-          furniture in your home
-        </h1>
-        <p className="article__text">
-          Ever been in a room and felt like something was missing? Perhaps it
-          felt slightly bare and uninviting. I’ve got some simple tips to help
-          you make any room feel complete.
-        </p>
-      </div>
-      <div className="article__author">Michelle Appleton 28 Jun 2020</div>
-      <ul className="article__socials">Share</ul>
-    </article>
+    <div className="article__wrapper">
+      <article className="article">
+        <img className="article__img" src={img} alt="Green drawer" />
+        <div className="article__container">
+          <main className="article__main">
+            <h1 className="article__title">
+              Shift the overall look and feel by adding these wonderful touches
+              to furniture in your home
+            </h1>
+            <p className="article__text">
+              Ever been in a room and felt like something was missing? Perhaps
+              it felt slightly bare and uninviting. I’ve got some simple tips to
+              help you make any room feel complete.
+            </p>
+          </main>
+          <footer className="article__footer">
+            <img
+              className="article__avatar"
+              src={avatar}
+              alt="Avatar of Michelle Appleton"
+            />
+            <div className="article__info">
+              <h2 className="article__author">Michelle Appleton</h2>
+              <time datetime="2020-05-28" className="article__date">
+                28 Jun 2020
+              </time>
+            </div>
+            <button className="article__button js-socials">Press me</button>
+          </footer>
+          <ul className="article__socials hidden">Share</ul>
+        </div>
+      </article>
+    </div>
   );
 }
 
